264a149e0521349f1074be49f88c6c20f9003e75
Jonatan <jontalas@gmail.com>
2021-04-07 16:11:22 +0200

    Practica: Laboratorio01
    Initial commit
    
    Changes to be committed:
          new file:   .vscode/launch.json
          new file:   css/site.css
          new file:   img/undraw_profile_pic_ic5t.svg
          new file:   index.html
          new file:   js/demo.js

---
+++ js/demo.js
@@ -0,0 +1,3 @@
+document.getElementById("name").value = "Mi nombre";
+document.getElementById("lastName").value = "Mi apellido";
+document.getElementById("avatar").src = "img/undraw_profile_pic_ic5t.svg";
